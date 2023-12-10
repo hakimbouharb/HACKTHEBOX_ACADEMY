66ed98ae04b45ab1826ba2e5c4c985a5550937f8
hakimbouharb <6869@holbertonstudents.com>
2023-12-10 09:33:30 -0500

    $[Dr.Wise]@(Kali)//:{HTB_ACADEMY}$

---
+++ var.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+advice = "Don't panic"
+ultimate_answer = 42
+potential_question = 6 * 7
+confident = True
+something_false = False
+problems = None
+# Oh, and by the way, this is a comment. We can tell by the leading # sign
+counter = 0
+
+while counter < 5:
+    print(f'Hello #{counter}')
+    counter = counter + 1
