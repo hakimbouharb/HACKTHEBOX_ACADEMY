fa80e2e2ef3dfb96aefe75ce5e1fbc6d8c3034a4
hakimbouharb <6869@holbertonstudents.com>
2023-12-10 09:49:50 -0500

    $[Dr.Wise]@(Kali)//:{HTB_ACADEMY}$

--- first_iteration.py
+++ first_iteration.py
@@ -1,8 +1,7 @@
+import click
 import requests
 import re
 from bs4 import BeautifulSoup
-
-PAGE_URL = 'http://94.237.51.118:51663'
 
 def get_html_of(url):
     resp = requests.get(url)
@@ -13,10 +12,12 @@ def get_html_of(url):
 
     return resp.content.decode()
 
-def count_occurrences_in(word_list):
+def count_occurrences_in(word_list, min_length):
     word_count = {}
 
     for word in word_list:
+        if len(word) < min_length:
+            continue
         if word not in word_count:
             word_count[word] = 1
         else:
@@ -30,12 +31,19 @@ def get_all_words_from(url):
     raw_text = soup.get_text()
     return re.findall(r'\w+', raw_text)
 
-def get_top_words_from(all_words):
-    occurrences = count_occurrences_in(all_words)
+def get_top_words_from(all_words, min_length):
+    occurrences = count_occurrences_in(all_words, min_length)
     return sorted(occurrences.items(), key=lambda item: item[1], reverse=True)
 
-all_words = get_all_words_from(PAGE_URL)
-top_words = get_top_words_from(all_words)
+@click.command()
+@click.option('--url', '-u', prompt='Web URL', help='URL of webpage to extract from.')
+@click.option('--length', '-l', default=0, help='Minimum word length (default: 0, no limit).')
+def main(url, length):
+    the_words = get_all_words_from(url)
+    top_words = get_top_words_from(the_words, length)
 
-for i in range(10):
-    print(top_words[i][0])
+    for i in range(10):
+        print(top_words[i][0])
+
+if __name__ == '__main__':
+    main()
