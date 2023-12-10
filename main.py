f87846cce9506921a92d04ce2b98c63f36a08a93
hakimbouharb <6869@holbertonstudents.com>
2023-12-10 09:35:51 -0500

    $[Dr.Wise]@(Kali)//:{HTB_ACADEMY}$

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    page_url = 'http://target:port'
+    the_words = get_all_words_from(page_url)
+    top_words = get_top_words_from(the_words)
+
+    for i in range(10):
+        print(top_words[i][0])
